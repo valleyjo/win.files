a1fe36ad1ace237dfcc2c79d0b540a9005a5e58e
Alex Vallejo <vallejo.alex@gmail.com>
2018-03-17 17:23:53 -0700

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,20 @@
-function shorten-path([string] $path) {
-   $loc = $path.Replace($HOME, '~')
-   # remove prefix for UNC paths
-   $loc = $loc -replace '^[^:]+::', ''
-   return $loc
+function Convert-PathToVimStyle([string] $path) {
    # make path shorter like tabs in Vim,
    # handle paths starting with \\ and . correctly
-   #return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2')
+   return ($path -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2')
+}
+
+function Format-Path([string] $path) {
+   $loc = $path.Replace($HOME, '~')
+   $loc = $path -ireplace 'd:\\one\\azure\\compute\\src', '%srcroot%'
+   # remove prefix for UNC paths
+   $loc = $loc -replace '^[^:]+::', ''
+   if ($loc -match "c:\\fcshell\\fcshell.release.\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\\lib\\net45" -Or
+       $loc -match "c:\\fcshell\\fcshell.release.\d{1,3}\.\d{1,3}\.\d{1,3}\\lib\\net45")
+   {
+       $loc = "FcShell"
+   }
+   return $loc
 }
 
 function prompt {
@@ -14,10 +23,12 @@ function prompt {
    $chost = [ConsoleColor]::Green
    $cloc = [ConsoleColor]::Cyan
 
-   write-host "$(Get-Date -Format G) " -n -f $cloc
-   write-host ($env:computername) -n -f $chost
-   write-host ' {' -n -f $cdelim
-   write-host (shorten-path (pwd).Path) -n -f $cloc
-   write-host '}' -n -f $cdelim
+   Write-Host
+   Write-Host "$(Get-Date -Format G) " -n -f $cloc
+   Write-Host ($env:computername) -n -f $chost
+   Write-Host ' {' -n -f $cdelim
+   Write-Host (Format-Path (Get-Location).Path) -n -f $cloc
+   Write-Host '}' -f $cdelim
+   Write-Host '$>' -n -f $chost
    return ' '
 }
